69867dff53398fc5f94d1121e74cae3f4b40bc4e
mohamed magdy <mohamed14magdi@gmail.com>
2019-04-27 01:20:05 +0200

    Headers

---
+++ header.h
@@ -0,0 +1,17 @@
+
+#define IDR_MYMENU 101
+#define ID_Exit   102
+#define ID_Load   103
+#define ID_Save   104
+#define ID_Circle 200
+#define ID_CircleCart 201
+#define ID_CirclePolar 202
+#define ID_LineDDA 301
+#define ID_LineMidPoint 302
+#define ID_RegularLine 303
+#define ID_ParaCurve 400
+#define ID_Parabola 401
+#define ID_Hermite 402
+#define ID_Bezier 403
+#define ID_LineClip 500
+
